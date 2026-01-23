53913e161cb5eeec69ee4d1dd6c14b5f1642fb38
namansharma05 <namanks0511@gmail.com>
2026-01-23 16:50:43 +0530

    added the bid function and test cases

--- bidding/programs/bidding/src/contexts.rs
+++ bidding/programs/bidding/src/contexts.rs
@@ -42,5 +42,40 @@ pub struct InitializeItem<'info> {
     )]
     pub item_account: Account<'info, Item>,
 
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + Escrow::INIT_SPACE,
+        seeds = [b"escrow", authority.key().as_ref(), item_counter_account.item_count.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub escrow_account: Account<'info, Escrow>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(item_id: u16)]
+pub struct Bid<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [
+            b"item",
+            item_id.to_le_bytes().as_ref(),
+        ],
+        bump,
+    )]
+    pub item_account: Account<'info, Item>,
+
+    #[account(
+        mut,
+        seeds = [b"escrow", item_account.authority.key().as_ref(), item_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub escrow_account: Account<'info, Escrow>,
+
     pub system_program: Program<'info, System>,
 }
--- bidding/tests/bidding.ts
+++ bidding/tests/bidding.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Bidding } from "../target/types/bidding";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 describe("  solana bidding dapp test cases",() => {
   // Configure the client to use the local cluster.
@@ -15,6 +16,7 @@ describe("  solana bidding dapp test cases",() => {
 
   let itemCounterAccountPda: anchor.web3.PublicKey;
   let itemAccountPda: anchor.web3.PublicKey;
+  let escrowAccountPda: anchor.web3.PublicKey;
 
 
   const findPda = (programId: anchor.web3.PublicKey, seeds: (Buffer | Uint8Array)[]): anchor.web3.PublicKey => {
@@ -27,38 +29,39 @@ describe("  solana bidding dapp test cases",() => {
     await provider.connection.confirmTransaction(tx);
   }
 
-  beforeEach(() => {
+  beforeEach(async() => {
     airDropSol(1000000000, adminWallet);
     airDropSol(1000000000, secondWallet);
+    itemCounterAccountPda = findPda(program.programId, [Buffer.from("item_counter")]);
   })
 
   it("should initialize item counter account successfully", async ()=> {
-    itemCounterAccountPda = findPda(program.programId, [Buffer.from("item_counter")]);
     const tx = await program.methods.initializeCounter().accounts({
       authority: adminWallet.publicKey,
       itemCounterAccount: itemCounterAccountPda,
     }).signers([adminWallet]).rpc();
     let itemCounterAccountData = await program.account.itemCounter.fetch(itemCounterAccountPda);
     console.log("    item counter when declared is", itemCounterAccountData.itemCount);
-    console.log("    Your transaction signature", tx);
+    // console.log("    Your transaction signature", tx);
   });
 
   it("should initialize item successfully", async () => {
     let itemCounterAccountData = await program.account.itemCounter.fetch(itemCounterAccountPda);
-    console.log("    item counter when item account is declared is", itemCounterAccountData.itemCount);
-    
     itemAccountPda = findPda(program.programId, [Buffer.from("item"), new anchor.BN(itemCounterAccountData.itemCount).toArrayLike(Buffer, "le", 2)]);
+    escrowAccountPda = findPda(program.programId, [Buffer.from("escrow"), adminWallet.publicKey.toBuffer(), new anchor.BN(itemCounterAccountData.itemCount).toArrayLike(Buffer, "le", 2)]);
     
     const name = "Item Name";
     const description = "This is the Item description";
     const imageUrl = "https://example.com/image.jpg";
-    const price = 100;
+    const price = 1000000000;
+    const minimumBid = 1000000000;
     
     console.log("    admin wallet public key is", adminWallet.publicKey.toBase58());
-    const tx = await program.methods.initializeItem(name, description, imageUrl, new anchor.BN(price)).accounts({
+    const tx = await program.methods.initializeItem(name, description, imageUrl, new anchor.BN(price/LAMPORTS_PER_SOL), new anchor.BN(minimumBid/LAMPORTS_PER_SOL)).accounts({
       authority: adminWallet.publicKey,
       itemCounterAccount: itemCounterAccountPda,
       itemAccount: itemAccountPda,
+      escrowAccount: escrowAccountPda,
     }).signers([adminWallet]).rpc();
 
     itemCounterAccountData = await program.account.itemCounter.fetch(itemCounterAccountPda);
@@ -66,13 +69,24 @@ describe("  solana bidding dapp test cases",() => {
     console.log("    Your transaction signature", tx);
     console.log("    item counter account pda", itemCounterAccountPda.toBase58());
     console.log("    item account pda", itemAccountPda.toBase58());
+    console.log("    escrow account pda", escrowAccountPda.toBase58());
+    console.log("    escrow account balance when initialized", await provider.connection.getBalance(escrowAccountPda));
   });
 
-  // it("should return all the accounts associated with the wallet address", async() => {
-  //   const accounts = await provider.connection.getAccountInfo(adminWallet.publicKey);
-  //   // accounts.forEach(element => {
-  //   //   console.log("    account pda/public key associated with admin wallet is",element.pubkey.toBase58());
-  //   // });
-  //   console.log("    all the associated accounts are", accounts);
-  // })
+  it("should bid successfully", async() => {
+    let itemCounterAccountData = await program.account.itemCounter.fetch(itemCounterAccountPda);
+    const itemId = itemCounterAccountData.itemCount - 1; // Assuming we bid on the last item created
+    
+    itemAccountPda = findPda(program.programId, [Buffer.from("item"), new anchor.BN(itemId).toArrayLike(Buffer, "le", 2)]);
+    // Note: Escrow PDA in this DApp seems to be derived using the creator's wallet (adminWallet)
+    escrowAccountPda = findPda(program.programId, [Buffer.from("escrow"), adminWallet.publicKey.toBuffer(), new anchor.BN(itemId).toArrayLike(Buffer, "le", 2)]);
+    
+    const tx = await program.methods.bid(itemId).accounts({
+      authority: adminWallet.publicKey,
+      itemAccount: itemAccountPda,
+      escrowAccount: escrowAccountPda,
+    }).signers([adminWallet]).rpc();
+    console.log("    Your transaction signature for bid", tx);
+    console.log("    escrow account balance after first bidding", await provider.connection.getBalance(escrowAccountPda));
+  });
 });
